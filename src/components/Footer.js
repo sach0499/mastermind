7370b40f60d95415935962dad344169778baeeb3
Sachin Pandey <sach0499@gmail.com>
2021-11-17 11:44:29 +0530

    Added Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="bottom-section">
+      <p>
+        Github Repo:{" "}
+        <a href="https://github.com/sach0499/mastermind-clone" target="blank" style={{ color: "#1A8D7C" }}>
+          Mastermind.
+        </a>
+      </p>
+      <p>
+        <a href="https://github.com/sach0499/" target="blank" style={{ color: "#1A8D7C" }}>
+          © Sachin Pandey
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
