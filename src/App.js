6f83a15363cdf846ef0a7042ce823e482f829126
Sachin Pandey <sach0499@gmail.com>
2021-11-17 18:16:25 +0530

    Added additional state variables

--- src/App.js
+++ src/App.js
@@ -8,12 +8,37 @@ import DecodingBoard from "./components/DecodingBoard";
 import CodePegs from "./components/CodePegs";
 import Footer from "./components/Footer";
 
-import {BASE_COLORS} from './utils/constants'
+import { generateBoardColors, generateHintsState } from "./utils/utils";
+
+import {
+  BASE_COLORS,
+  FIRST_ROUND,
+  NUM_BLOCKS,
+  NUM_PEGS,
+  DEFAULT_PEG_COLOR,
+  DEFAULT_HINT_STATE,
+} from "./utils/constants";
+
+const INITIAL_BOARD_COLORS = generateBoardColors(
+  NUM_BLOCKS,
+  NUM_PEGS,
+  DEFAULT_PEG_COLOR
+);
+const INITIAL_HINTS_STATE = generateHintsState(
+  NUM_BLOCKS,
+  NUM_PEGS,
+  DEFAULT_HINT_STATE
+);
 
 function App() {
+  const [boardColors, setBoardColors] = useState(INITIAL_BOARD_COLORS);
+  const [boardHintsState, setBoardHintsState] = useState(INITIAL_HINTS_STATE);
+  const [currentRound, setCurrentRound] = useState(FIRST_ROUND);
+  const [countPegsFilled, setPegsSlotsFilled] = useState(0);
   const [selectedColor, setSelectedColor] = useState(BASE_COLORS[0]);
   const [gameOver, setGameOver] = useState(false);
   const [gameWon, setGameWon] = useState(false);
+
 
   const selectColorHandler = (color) => {
     setSelectedColor(color);
@@ -31,8 +56,18 @@ function App() {
       <img className="logo" src="/assets/images/Logo.png" alt="logo" />
       <Rules />
       <div className="main-section">
-        <DecodingBoard />
-        <CodePegs colors={BASE_COLORS} selectedColor={selectedColor} onSelectColor={selectColorHandler} />
+        <DecodingBoard
+          numBlocks={NUM_BLOCKS}
+          boardColors={boardColors}
+          boardHintsState={boardHintsState}
+          currentRound={currentRound}
+          allPegsFilled={countPegsFilled === NUM_PEGS}
+        />
+        <CodePegs
+          colors={BASE_COLORS}
+          selectedColor={selectedColor}
+          onSelectColor={selectColorHandler}
+        />
       </div>
       <Footer />
     </div>
