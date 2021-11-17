552771e4e14414d7d84082287e2aa0b301e68381
Sachin Pandey <sach0499@gmail.com>
2021-11-17 12:09:00 +0530

    Added game constants

---
+++ src/utils/constants.js
@@ -0,0 +1,40 @@
+export const BASE_COLORS = [
+  "#FFB400",
+  "#FF5A5F",
+  "#8CE071",
+  "#00D1C1",
+  "#007A87",
+  "#7B0051",
+];
+
+export const DEFAULT_COLOR = "#f4f4f4";
+
+export const LAST_ROUND = 9;
+export const FIRST_ROUND = 0;
+
+export const INITIAL_BLOCK_COLORS = [
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+  ["#f4f4f4", "#f4f4f4", "#f4f4f4", "#f4f4f4"],
+];
+
+export const INITIAL_PEGS_STATE = [
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+  ["unchecked", "unchecked", "unchecked", "unchecked"],
+];
