99394c3a7d1c58259266c588d76b78f6a9a0aa7a
Sachin Pandey <sach0499@gmail.com>
2021-11-17 11:50:29 +0530

    Added game states and hooked up Modal with App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 import "./App.css";
 
-import Modal from './components/Modal';
-import Rules from './components/Rules';
-import DecodingBoard from './components/DecodingBoard';
-import CodePegs from './components/CodePegs'
-import Footer from './components/Footer'
+import Modal from "./components/Modal";
+import Rules from "./components/Rules";
+import DecodingBoard from "./components/DecodingBoard";
+import CodePegs from "./components/CodePegs";
+import Footer from "./components/Footer";
 
 function App() {
+  const [gameOver, setGameOver] = useState(false);
+  const [gameWon, setGameWon] = useState(false);
+
+  const resetGameHandler = () => {
+  };
+
   return (
     <div className="container">
-      <Modal/>
+      <Modal
+        gameOver={gameOver}
+        gameWon={gameWon}
+        onResetGame={resetGameHandler}
+      />
       <img className="logo" src="/assets/images/Logo.png" alt="logo" />
       <Rules />
       <div className="main-section">
-        <DecodingBoard/>
-        <CodePegs/>
+        <DecodingBoard />
+        <CodePegs />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
