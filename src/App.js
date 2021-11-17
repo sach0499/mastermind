b6eb89e4202db168a329ad166eb565591b04bbff
Sachin Pandey <sach0499@gmail.com>
2021-11-17 11:13:28 +0530

    Added the component structure and component files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from 'react';
+
 import "./App.css";
 
+import Modal from './components/Modal';
+import Rules from './components/Rules';
+import DecodingBoard from './components/DecodingBoard';
+import CodePegs from './components/CodePegs'
+
 function App() {
-  return <p> Hello world</p>;
+  return (
+    <div className="container">
+      <Modal/>
+      <img className="logo" src="/assets/images/Logo.png" alt="logo" />
+      <Rules />
+      <div className="main-section">
+        <DecodingBoard/>
+        <CodePegs/>
+      </div>
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
