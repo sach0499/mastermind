333ead9b297ad1cd01a979c2a970d081e1aabcd1
Sachin Pandey <sach0499@gmail.com>
2021-11-17 18:16:06 +0530

    Added Decoding Board Component

--- src/components/DecodingBoard.js
+++ src/components/DecodingBoard.js
@@ -1,7 +1,37 @@
 import React from "react";
 
-function DecodingBoard() {
-    return null;
+import Block from "./Block";
+
+import './DecodingBoard.css'
+
+function DecodingBoard({
+  numBlocks,
+  boardColors,
+  boardHintsState,
+  selectedColor,
+  currentRound,
+  onClickPeg,
+  allPegsFilled,
+  onClickSubmitButton,
+}) {
+  return (
+    <div className="block-container">
+      {[...Array(numBlocks)].map((el, blockId) => (
+        <Block
+          blockColor={boardColors[blockId]}
+          blockHintsState={boardHintsState[blockId]}
+          isBlockSelected={blockId === currentRound}
+          isBlockProcessed={blockId < currentRound}
+          selectedColor={selectedColor}
+          allPegsFilled={allPegsFilled}
+          onClickPeg={onClickPeg}
+          onClickSubmitButton={onClickSubmitButton}
+          key={blockId}
+          blockId={blockId}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default DecodingBoard;
